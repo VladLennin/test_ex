174f1ce932bcf093314aad2def3e6320f6bb183b
vladlenmarchenko <markelovwtf7@gmail.com>
2022-04-22 23:28:14 +0300

    Upper block ready

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles/app.css'
+import UpperBlock from "./components/UpperBlock";
+function App() {
+    return (
+        <div className="App">
+            <UpperBlock/>
+        </div>
+    );
+}
+
+export default App;
